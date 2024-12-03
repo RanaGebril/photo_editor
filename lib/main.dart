a5aee9f0c71a39975ef00f84289dbe3ae1faaa7a
Rana_Gebril <ranagebril5@gmail.com>
2024-12-03 17:32:20 +0200

    splash

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:photo_editor/home_screen.dart';
+import 'package:photo_editor/splash_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: SplashScreen(),
+        debugShowCheckedModeBanner: false,
+        routes: {
+          HomeScreen.routeName: (context) => HomeScreen(),
+
+        },
+      );
+  }
+}
